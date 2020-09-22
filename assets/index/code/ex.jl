c67bdf0b557fa8cab69dbcae9d121dd1f85dd4e4
nignatiadis <nikos.ignatiadis01@gmail.com>
2020-09-22 00:19:28 +0000

    Deploying to gh-pages from  @ 312a13398dfe29b1e73b45e25ca684ae0e364ea5 🚀

---
+++ assets/index/code/ex.jl
@@ -0,0 +1,3 @@
+# This file was generated, do not modify it. # hide
+plot(running_var; ylim=(0,600), bins=40, background_color="#f3f6f9", size=(600,300))
+savefig(joinpath(@OUTPUT, "histogram.svg")) # hide
