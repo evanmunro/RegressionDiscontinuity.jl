be062a4eb453a1eec48bd54e1cc4e4e4db9e82ee
nignatiadis <nikos.ignatiadis01@gmail.com>
2020-09-22 01:17:19 +0000

    Deploying to gh-pages from  @ 65fa52c62c40080e5c0b3114a98a8684784e99e7 🚀

--- assets/index/code/ex.jl
+++ assets/index/code/ex.jl
@@ -1,3 +1,3 @@
 # This file was generated, do not modify it. # hide
-plot(running_var; ylim=(0,600), bins=40, background_color="#f3f6f9", size=(600,300))
-savefig(joinpath(@OUTPUT, "histogram.svg")) # hide
+triang_ll_rd = fit(NaiveLocalLinearRD(kernel=SymTriangularDist(), bandwidth=ImbensKalyanaraman()),
+				   running_var, lee08.Ys)
